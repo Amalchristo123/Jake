5ef548482b2e635dc6465d70797dc75669034b28
ZINAN <97530806+Zinan100@users.noreply.github.com>
2023-08-06 10:02:41 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ import re
 import os
 from datetime import datetime
 from typing import List
+from database.join_reqs import JoinReqs as db2
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
 import requests
